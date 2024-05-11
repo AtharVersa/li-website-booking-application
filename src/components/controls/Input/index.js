43587e441863da6c40a7dbd8a73e83daaeb24922
AtharVersa <athar@versamicro.com>
2024-05-11 20:12:43 +0530

    line formatted

--- src/components/controls/Input/index.js
+++ src/components/controls/Input/index.js
@@ -1,20 +1,52 @@
-import React from 'react'
+import React, { memo } from "react";
 
 const styles = {
-    color: '#F00',
-    fontSize: '12px'
-}
+	color: "#F00",
+	fontSize: "12px",
+};
 
-export default function Input ({ label, type, register, placeholder, name, error, required }) {
-    return (
-        <>
-        {label && <label htmlFor="input-field" className="font-weight-bold text-primary">{label}</label>}
-            <input type={type} className={'form-control' + (!error ?  '' :' is-invalid')}
-                placeholder={placeholder}
-                name={name}
-                {...register(name, required )}
-                />
-            {error && <span style={styles}>{error}</span>}
-        </>
-    )
+function Input({ label, type, register, placeholder, name, error, required }) {
+	function getCurrentDate() {
+		if (type === "date") {
+			const today = new Date();
+			const year = today.getFullYear();
+			const month = String(today.getMonth() + 1).padStart(2, "0");
+			const day = String(today.getDate()).padStart(2, "0");
+			return `${year}-${month}-${day}`;
+		}
+	}
+
+	return (
+		<>
+			{label && (
+				<label
+					htmlFor="input-field"
+					className="font-weight-bold text-primary"
+				>
+					{label}
+				</label>
+			)}
+
+			{type === "date" ? (
+				<input
+					type={type}
+					className={"form-control" + (!error ? "" : " is-invalid")}
+					placeholder={placeholder}
+					name={name}
+					min={getCurrentDate()}
+					{...register(name, required)}
+				/>
+			) : (
+				<input
+					type={type}
+					className={"form-control" + (!error ? "" : " is-invalid")}
+					placeholder={placeholder}
+					name={name}
+					{...register(name, required)}
+				/>
+			)}
+			{error && <span style={styles}>{error}</span>}
+		</>
+	);
 }
+export default memo(Input);
